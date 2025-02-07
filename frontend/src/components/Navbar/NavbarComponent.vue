2f0484e3f0354dc9eb3f5f330a79c9ea575ef3df
Bogdan Mihaica <claudiumihaica12@gmail.com>
2025-02-07 11:37:41 +0200

    Navbar style

---
+++ frontend/src/components/Navbar/NavbarComponent.vue
@@ -0,0 +1,17 @@
+<script>
+import NavbarLink from './NavbarLink.vue';
+
+export default {
+    components: { NavbarLink }
+}
+</script>
+
+<template>
+    <div class="top-0 w-full h-15 bg-violet-700 rounded-b-2xl fixed flex justify-start items-center">
+        <div class="flex gap-5">
+            <NavbarLink route="/" icon="fas fa-house" class="first:ml-5">Home</NavbarLink>
+            <NavbarLink route="/cart" icon="fas fa-cart-shopping">Cart</NavbarLink>
+            <NavbarLink route="/login" icon="fas fa-right-to-bracket">Login</NavbarLink>
+        </div>
+    </div>
+</template>
---
+++ frontend/src/components/Navbar/NavbarLink.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    props: {
+        route: String,
+        icon: String
+    }
+}
+</script>
+
+<template>
+    <div class="">
+        <router-link :to="route" class="text-white rounded-lg min-w-20 px-4 py-2 bg-violet-800 transition-all duration-500 flex justify-center items-center text-lg
+                    hover:bg-violet-600 ">
+            <i :class="icon" class="mr-2 text-sm"></i>
+            <slot></slot>
+            
+        </router-link>
+    </div>
+</template>
